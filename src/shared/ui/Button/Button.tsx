696844c1992002588cc73ed42e0f5c123d706592
Irina Kirillova <keira.kirillova@gmail.com>
2024-07-13 18:45:31 +0200

    add shared ui component Button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import { classNames } from 'shared/lib/helpers/classNames';
+import * as classes from './Button.module.scss';
+
+export enum ButtonStyle {
+  TEXT = 'text',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonStyle?: ButtonStyle;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { className, buttonStyle, children, ...otherProps } = props;
+
+  return (
+    <button className={classNames(classes.btn, [className, classes[buttonStyle]])} {...otherProps}>
+      {children}
+    </button>
+  )
+}
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import { Theme, useTheme } from 'app/context/theme';
-import { classNames } from 'shared/lib/helpers/classNames';
+import { Button, ButtonStyle } from 'shared/ui/Button/Button';
 import Sun from 'shared/assets/icons/sun.svg';
 import Moon from 'shared/assets/icons/moon.svg';
 import * as classes from './ThemeSwitcher.module.scss';
@@ -8,8 +8,8 @@ export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button className={classNames(classes.theme)} onClick={toggleTheme}>
+    <Button className={classes.theme} buttonStyle={ButtonStyle.TEXT} onClick={toggleTheme}>
       {theme === Theme.LIGHT ? <Moon width="36" height="36" /> : <Sun width="36" height="36" />}
-    </button>
+    </Button>
   )
 }
