87be4d780f23d488bd3535593a1b6875e6b0c800
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-08-28 22:37:59 +0200

    add eslint for i18n

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import i18next from 'eslint-plugin-i18next'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -17,6 +18,12 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    plugins: { i18next },
+    rules: {
+      'i18next/no-literal-string': ['error', { mode: 'jsx-text-only' }]
+    }
+  },
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
